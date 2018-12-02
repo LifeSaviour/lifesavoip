762673434293558e3e4c66cd6bc748b80e5dc405
LifeSaviour <45505527+LifeSaviour@users.noreply.github.com>
2018-12-01 22:59:32 -0800

    dasd
    dasdasd

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,6 +3,38 @@ import React, { Component } from 'react';
 import firebase from '../Config/firebase';
 import Form from '../Components/Form';
 import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+    list: {
+        width: 250,
+      },
+      fullList: {
+        width: 'auto',
+      }
+  }
 
 class Login extends Component {
 constructor(){
@@ -20,9 +52,16 @@ constructor(){
      errormessagesignup:"",
      phoneno:"",
      branch:false,
-     branchname:''
+     branchname:'',
+     left:false
     }
 }
+
+toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
 
 login=()=>{
     firebase.auth().signInWithEmailAndPassword(this.state.emailsignin,this.state.passwordsignin).then((user)=>{
@@ -93,11 +132,34 @@ seterrorsignin=()=>{
 
 
 render(){
+    
+        
     return(
         <div>
 
             {this.state.user==true && <div>
-                <Button onClick={()=>{this.signout()}} variant="contained" color="primary">Logout</Button>
+                <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+                <div className={styles.root}>
+                <AppBar position="static">
+                <Toolbar>
+                <IconButton className={styles.menuButton} color="inherit" aria-label="Menu" > >
+            <MenuIcon/>
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={styles.grow}>
+                Life Saviour
+                </Typography>
+                <Button onClick={()=>{this.signout()}} style={{float:"left"}} variant="contained" color="primary">Logout</Button>
+                </Toolbar>
+                </AppBar>
+                </div>
                 <Form/>
             </div>}
 {this.state.user==false && <div>
